d1145b4eaf66286d056712c2fc130b5d1c8f90dc
yamkel-cell <156504328+yamkel-cell@users.noreply.github.com>
2025-05-06 09:47:27 +0200

    Create auth.js

---
+++ netlify/functions/auth.js
@@ -0,0 +1,11 @@
+const { handler } = require('netlify-cms-oauth-provider-node');
+
+exports.handler = handler({
+  // These should match what you set in Auth0
+  provider: 'auth0',
+  clientId: '2Ud5AqjVGaxCs1SRjti4jg32AMeK0HNC',
+  clientSecret: 'Ag7OVGQKGjZwtv9GEuXKq5MFdZewBbTFAfLad94UnAMtr5Fi1tNnSfnTCR6eJaCe',
+  authorizeUrl: 'https://dev-piuhjo7tbh61jome.us.auth0.com/authorize',
+  accessTokenUrl: 'https://dev-piuhjo7tbh61jome.us.auth0.com/oauth/token',
+  userUrl: 'https://dev-piuhjo7tbh61jome.us.auth0.com/userinfo',
+});
