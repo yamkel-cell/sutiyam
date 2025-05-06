b4f564bc272557b294f55f8f91b47ac9b75845a7
yamkel-cell <156504328+yamkel-cell@users.noreply.github.com>
2025-05-06 10:22:59 +0200

    Update auth.js

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -1,7 +1,6 @@
 const handler = require('netlify-cms-oauth-provider-node');
 
 exports.handler = handler({
-  // These should match what you set in Auth0
   provider: 'auth0',
   clientId: '2Ud5AqjVGaxCs1SRjti4jg32AMeK0HNC',
   clientSecret: 'Ag7OVGQKGjZwtv9GEuXKq5MFdZewBbTFAfLad94UnAMtr5Fi1tNnSfnTCR6eJaCe',
