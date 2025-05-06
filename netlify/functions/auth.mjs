40113470a7fd72aa5da6eb6c66f35f2ef75f0837
yamkel-cell <156504328+yamkel-cell@users.noreply.github.com>
2025-05-06 10:30:08 +0200

    Update and rename auth.js to auth.mjs

--- netlify/functions/auth.mjs
+++ netlify/functions/auth.mjs
@@ -1,10 +1,13 @@
-const handler = require('netlify-cms-oauth-provider-node');
+import { createBeginHandler, createCompleteHandler } from 'netlify-cms-oauth-provider-node';
 
-exports.handler = handler({
+const config = {
   provider: 'auth0',
   clientId: '2Ud5AqjVGaxCs1SRjti4jg32AMeK0HNC',
   clientSecret: 'Ag7OVGQKGjZwtv9GEuXKq5MFdZewBbTFAfLad94UnAMtr5Fi1tNnSfnTCR6eJaCe',
   authorizeUrl: 'https://dev-piuhjo7tbh61jome.us.auth0.com/authorize',
   accessTokenUrl: 'https://dev-piuhjo7tbh61jome.us.auth0.com/oauth/token',
   userUrl: 'https://dev-piuhjo7tbh61jome.us.auth0.com/userinfo',
-});
+};
+
+export const begin = createBeginHandler(config);
+export const complete = createCompleteHandler(config);
